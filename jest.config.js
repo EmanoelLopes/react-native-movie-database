320bac36bd5b711da57822cf514f398eb36a85ae
emanoell <emanoel.lopes.web@gmail.com>
2021-05-09 13:53:30 -0300

    test: add collectCoverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/constants/**",
+    "!<rootDir>/src/services/**",
+    "!<rootDir>/src/utils/**"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
 };
